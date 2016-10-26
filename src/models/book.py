2eddfd31c6f39ae92b65fc46c142f3bf985b0d6d
Piotr Kubicki <40281429@live.napier.ac.uk>
2016-10-26 19:43:45 +0000

    report updated, title added

--- src/models/book.py
+++ src/models/book.py
@@ -99,13 +99,6 @@ class Book():
       self.db.cursor().execute('INSERT INTO book_author VALUES (?, ?)', data)
       self.db.commit()
 
-#default_text =  '''Praesent aliquet mattis tellus, ac pulvinar turpis faucibus sit amet.
-# Aenean metus eros, dignissim ac pellentesque et, tempor at risus. Praesent egestas sapien
-# convallis leo volutpat egestas. Nulla at tristique purus. Donec tristique
-# molestie scelerisque. Vestibulum ultrices consequat semper. Cras id felis faucibus,
-# eleifend eros vitae, ornare diam. Etiam vel nisi tempus, fermentum nibh nec,
-# elementum neque. Nulla facilisi. Mauris vitae lobortis quam, at rutrum magna.'''
-
 default_text = 'Nulla laoreet, tortor eget sagittis pulvinar, leo risus feugiat nisl, at condimentum erat turpis non nisl. Donec ac leo ultrices, aliquet magna ultricies, efficitur est. Donec pharetra lobortis urna. Nunc arcu est, posuere et diam sit amet, ultrices lobortis leo. Sed eu urna non neque ultrices malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi commodo, nisl eu viverra euismod, tortor neque hendrerit metus, sit amet efficitur nisl nunc in nisi. Donec id accumsan purus. Ut sit amet mi gravida, tempor magna porttitor, egestas nulla. Donec iaculis massa vitae dui auctor, eget placerat justo scelerisque. Aenean scelerisque nunc vestibulum felis accumsan tempor. Donec eu ornare nibh, ut interdum magna.'
 
 
